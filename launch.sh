52e8500544046817002f66096241c21551f610b7
ivfiev <filjko@ya.ru>
2023-10-08 20:21:34 +0100

    launch script

---
+++ launch.sh
@@ -0,0 +1,8 @@
+
+if [[ $(docker container ls -a | grep 'stable_diffusion') == *stable_diffusion* ]]; then
+    docker start -i stable_diffusion
+else
+    docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add=video --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+    -v $HOME/stable-diffusion:/stable-diffusion --name stable_diffusion \
+    stable_diffusion
+fi
