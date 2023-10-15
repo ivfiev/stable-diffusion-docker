45de2abb2502b01a1ed61b60f4ae0820b69b38bb
ivfiev <filjko@ya.ru>
2023-10-15 19:38:41 +0100

    mount gradio

--- launch.sh
+++ launch.sh
@@ -3,6 +3,8 @@ if [[ $(docker container ls -a | grep 'stable_diffusion') == *stable_diffusion* 
     docker start -i stable_diffusion
 else
     docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add=video --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-    -v $HOME/stable-diffusion:/stable-diffusion --name stable_diffusion \
+    -v $HOME/stable-diffusion:/stable-diffusion \
+    -v $HOME/stable-diffusion/stable-diffusion-webui/outputs/gradio:/tmp/gradio \
+    --name stable_diffusion \
     stable_diffusion
 fi
