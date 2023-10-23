21a85130d69ddd40a4d0373b88d83a97ecce5235
ivfiev <filjko@ya.ru>
2023-10-23 23:21:15 +0100

    mount gradio onto RAM

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ if [[ $(docker container ls -a | grep 'stable_diffusion') == *stable_diffusion* 
 else
     docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add=video --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
     -v $HOME/stable-diffusion:/stable-diffusion \
-    -v $HOME/stable-diffusion/stable-diffusion-webui/outputs/gradio:/tmp/gradio \
+    -v /run/stable-diffusion/gradio:/tmp/gradio \
     --name stable_diffusion \
     stable_diffusion
 fi
